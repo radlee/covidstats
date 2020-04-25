e8a005f38956e4be118b07c9adec35cf49f7f8f9
radlee <prof.lolito@gmail.com>
2020-04-25 14:52:38 +0200

    Updates

---
+++ precache-manifest.ecaf62d172d2caf9aad78bed04d3e68d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa20879c75c50ec52328f987869c4b9",
+    "url": "/covidstats/index.html"
+  },
+  {
+    "revision": "7a103301bad087ebbc32",
+    "url": "/covidstats/static/css/main.7a760f23.chunk.css"
+  },
+  {
+    "revision": "e92b4bccb7da57509bd9",
+    "url": "/covidstats/static/js/2.99a87ff2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covidstats/static/js/2.99a87ff2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a103301bad087ebbc32",
+    "url": "/covidstats/static/js/main.d7ce0d3c.chunk.js"
+  },
+  {
+    "revision": "29877b3c5c50cb22c0ff",
+    "url": "/covidstats/static/js/runtime-main.915cac82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covidstats/static/media/logo.5d5d9eef.svg"
+  }
+]);
